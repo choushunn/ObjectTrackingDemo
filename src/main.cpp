df56197cd973c1ce717cae636ea5c7852aac5a5a
choushunn <choushunn@stu.cqut.edu.cn>
2023-05-17 19:47:14 +0800

    ui

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,17 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
-
+#include <QStyleFactory>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
+    //系统自带的QStyle风格
+    QStringList listStyle = QStyleFactory::keys();
+    //打印当前系统支持的系统风格
+    foreach(QString val, listStyle)
+        qDebug()<<val<<"  ";
+    //设置当前风格为
+    qApp->setStyle(QStyleFactory::create("Fusion"));
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
